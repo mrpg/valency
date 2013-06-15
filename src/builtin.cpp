f689ec981ebcebd3d9c0786f1a843ba60244f5fa
Max R. P. Grossmann <99bb251c1df4174701b02c7acb7e08fd@1e5b.de>
2013-06-15 20:15:28 +0200

    bugfix

--- src/builtin.cpp
+++ src/builtin.cpp
@@ -1184,7 +1184,7 @@ void builtin_readlines(vector<shared_ptr<instr_t>>& arg) {
 			int64_t i = 0;
 
 			while (getline(cin,tmp)) {
-				((vlist*)arg[1]->p)->push_back(pair<instr_t*,instr_t*>(get(new int64_t(i),XNUMT),get(new string(tmp),XSTRINGT)));
+				((vlist*)arg[1]->p)->push_back(pair<shared_ptr<instr_t>,shared_ptr<instr_t>>(shared_ptr<instr_t>(get(new int64_t(i),XNUMT)),shared_ptr<instr_t>(get(new string(tmp),XSTRINGT))));
 				i++;
 			}
 		}
@@ -1212,13 +1212,13 @@ void builtin_range(vector<shared_ptr<instr_t>>& arg) {
 
 			if (b >= a) {
 				for (; a <= b; a += step) {
-					dest->push_back(pair<instr_t*,instr_t*>(get(new int64_t(i),XNUMT),get(new double(a),XFLOATT)));
+					dest->push_back(pair<shared_ptr<instr_t>,shared_ptr<instr_t>>(shared_ptr<instr_t>(get(new int64_t(i),XNUMT)),shared_ptr<instr_t>(get(new double(a),XFLOATT))));
 					i++;
 				}
 			}
 			else {
 				for (; a >= b; a += step) {
-					dest->push_back(pair<instr_t*,instr_t*>(get(new int64_t(i),XNUMT),get(new double(a),XFLOATT)));
+					dest->push_back(pair<shared_ptr<instr_t>,shared_ptr<instr_t>>(shared_ptr<instr_t>(get(new int64_t(i),XNUMT)),shared_ptr<instr_t>(get(new double(a),XFLOATT))));
 					i++;
 				}
 			}
